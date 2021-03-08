25583f53f63bbf5e9919ae5d632ebda90c0acf39
Natalija-J <77069086+Natalija-J@users.noreply.github.com>
2021-03-08 21:26:32 +0200

    Add project files.

---
+++ CinemaBookingSystem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CinemaBookingSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
