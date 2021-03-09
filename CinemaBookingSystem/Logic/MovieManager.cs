c54fadf7880c605be4d4cafdd4f18ecb04696e6f
Natalija-J <77069086+Natalija-J@users.noreply.github.com>
2021-03-09 11:26:08 +0200

    get movies by category

--- CinemaBookingSystem/Logic/MovieManager.cs
+++ CinemaBookingSystem/Logic/MovieManager.cs
@@ -8,13 +8,30 @@ namespace CinemaBookingSystem.Logic
 {
     public class MovieManager
     {
-        public List<Movies> GetMovies(int count = 6)
+        //getting movies that are playing now
+        public List<Movies> GetMoviesPlayingToday(int count = 6)
         {
             using (var db = new CinemaDb())
             {
-                // SELECT TOP 6 * FROM Articles ORDER BY PublishedOn DESC
-                return db.Movies.OrderBy(m => m.PlayingTime).Take(count).ToList();
+                // SELECT TOP 6 * FROM Movies ORDER BY Playing date - today
+                return db.Movies.OrderBy(m => m.PlayingTime ==DateTime.Today).Take(count).ToList();
             }
         }
+        //get movies by their category
+        public List<Movies> GetMoviesByCategory(string category)
+        {
+            using(var db = new CinemaDb())
+            {
+                return db.Movies.Where(m => m.Category.ToLower() == category.ToLower()).ToList();
+            }
+        }
+        // getting movies that are coming soon
+        //public List<Movies> GetComingSoonMovies()
+        //{
+        //    using(var db = new CinemaDb())
+        //    {
+        //        return db.Movies.Where(m => m.PlayingTime < DateTime.Today()).ToList();
+        //    }
+        //}
     }
 }
